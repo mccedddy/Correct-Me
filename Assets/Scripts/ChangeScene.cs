82750fc33f60c88b5f1a84fd71f419b96cb96a80
Daniel John Cedric Macallan <cedricmacallan@gmail.com>
2022-06-15 14:32:15 +0800

    Add change scene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // LoadAsync
+    public void BeginLoadLevel()
+    {
+        StartCoroutine(LoadMenuAsync());
+    }
+    private IEnumerator LoadMenuAsync()
+    {
+        var progress = SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Additive);
+        while (!progress.isDone)
+        {
+            // Check each frame if the scene has completed
+            yield return null;
+        }
+
+        // Code after this point is executed after the new scene has loaded
+        SceneManager.LoadScene("MainMenu");
+    }
+}
